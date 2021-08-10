cd8a14843eafe81b86e78699e300bb8158242175
oshingate <oshingate@gmail.com>
2021-08-10 16:54:55 +0530

    Added error Boundary to conduit

--- src/utils/ErrorBoundary.js
+++ src/utils/ErrorBoundary.js
@@ -11,17 +11,17 @@ class ErrorBoundary extends Component {
   }
 
   componentDidCatch(error, info) {
+    console.dir(error);
     this.setState({ hasError: true, error, info });
   }
 
   render() {
     if (this.state.hasError) {
       return (
-        <section>
-          <h2 className='sec-heading text-center'>
-            OOPS !!!!!! {this.state.error}
-          </h2>
-          <p>{this.state.info}</p>
+        <section className='hero-sec'>
+          <h3 className='sec-heading text-center'>
+            OOPS !!!!!! {this.state.error.message}
+          </h3>
         </section>
       );
     } else {
